29978937388748f9471479b8ae503a8a44e49ef0
hubo00 <hub.bukowski@gmail.com>
2022-12-18 20:24:52 +0000

    Reformatting & Styling

--- scripts.js
+++ scripts.js
@@ -1,45 +1,12 @@
-// Script which listens for scrolling events, and takes the user directly to the section
-$(document).ready(function () {
-  var currentSection = 1;
-  $(window).on("wheel mousewheel", function (e) {
-    if (e.originalEvent.deltaY > 0) {
-      // Scrolling down
-      if (currentSection < 4) {
-        currentSection++;
-        $("html, body").animate(
-          {
-            scrollTop: $("#section-" + currentSection).offset().top,
-          },
-          1000
-        );
-      }
-    } else {
-      // Scrolling up
-      if (currentSection > 1) {
-        currentSection--;
-        $("html, body").animate(
-          {
-            scrollTop: $("#section-" + currentSection).offset().top,
-          },
-          1000
-        );
-      }
-    }
-  });
-});
-
 $(document).ready(function () {
   $('.carousel').carousel()
 })
 
 function sendEmail() {
-  // Get the values of the subject and content fields
   const subject = document.getElementById('subjectInput').value;
   const body = document.getElementById('contentInput').value;
 
-  // Construct the mailto: URL with the subject and body
   const url = `mailto:hub.bukowski@gmail.com?subject=${subject}&body=${body}`;
 
-  // Open the URL in a new window
   window.open(url);
 }
